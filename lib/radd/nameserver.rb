13e731a9b72722d63710a06bcf65cdc4fed61248
Matthias Grosser <mtgrosser@gmx.net>
2026-05-04 00:26:22 +0200

    Refactor nameserver

--- lib/radd/nameserver.rb
+++ lib/radd/nameserver.rb
@@ -7,23 +7,39 @@ module Radd
 
     def process(name, resource_class, transaction)
       name = Resolv::DNS::Name.create(name.downcase)
-      # NOTE: do not use case..when, as resource classes are not identical
-      if Resolv::DNS::Resource::IN::A == resource_class
-        if Radd.mname == name || Radd.origin == name
-          return respond_a(transaction, Radd.ip)
-        elsif name.subdomain_of?(Radd.origin)
-          return respond_a(transaction, Radd.query(name))
+      type = query_type(resource_class)
+      if Radd.origin == name
+        case type
+        when :A   then return respond_a(transaction, Radd.ip)
+        when :SOA then return transaction.respond!(Radd.mname, Radd.rname, Radd.serial, 10800, 1800, 604800, 1800)
+        when :NS  then return transaction.respond!(Radd.mname)
         end
-      elsif resource_class <= Resolv::DNS::Resource::SOA && Radd.origin == name
-        # mname, rname, serial, refresh, retry_, expire, minimum
-        return transaction.respond!(Radd.mname, Radd.rname, Radd.serial, 10800, 1800, 604800, 1800)
-      elsif resource_class <= Resolv::DNS::Resource::NS && Radd.origin == name
-        return transaction.respond!(Radd.mname)
+      elsif name.subdomain_of?(Radd.origin)
+        case type
+        when :A then return respond_a(transaction, Radd.query(name))
+        when :MX, :AAAA then return transaction.fail!(:NoError)
+        end
       end
       transaction.fail!(:Refused)
     end
 
     private
+
+    def query_type(resource_class)
+      if Resolv::DNS::Resource::IN::A == resource_class
+        :A
+      elsif resource_class <= Resolv::DNS::Resource::SOA
+        :SOA
+      elsif resource_class <= Resolv::DNS::Resource::NS
+        :NS
+      elsif resource_class <= Resolv::DNS::Resource::MX
+        :MX
+      elsif resource_class <= Resolv::DNS::Resource::IN::AAAA
+        :AAAA
+      else
+        :UNKNOWN
+      end
+    end
 
     def respond_a(transaction, ip)
       if ip
--- lib/radd/version.rb
+++ lib/radd/version.rb
@@ -1,3 +1,3 @@
 module Radd
-  VERSION = '1.6.0'
+  VERSION = '1.6.1'
 end
